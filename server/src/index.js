ce05dcdcc626ab0d088669584861b1012b42a408
code-with-h <hamimk095@gmail.com>
2026-08-03 22:19:12 +0600

    Connect MongoDB Atlas

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,23 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server is running on port ${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`);
+      console.log(`🌍 Environment: ${process.env.NODE_ENV}`);
+    });
+  } catch (error) {
+    console.error("Server startup failed");
+    console.error(error);
+  }
+};
+
+startServer();
